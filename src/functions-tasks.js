7bb813d210400e0914fc9f5a7eeb3cfd639a78f5
vahagn85 <v.ulikhanyan@yandex.ru>
2025-04-09 22:31:01 +0400

    feat: implement getArgumentsCount

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,8 +50,13 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const arr = [];
+  funcs.forEach((f) => {
+    arr.push(f.length);
+  });
+
+  return arr;
 }
 
 /**
